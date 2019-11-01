fb75f13437be2294862fc415482114a13cb8172e
khai <khai.phanvan.nt@gmail.com>
2019-11-01 17:43:26 +0700

    Fix bug

--- plugin.js
+++ plugin.js
@@ -12,35 +12,39 @@
 
   var slugify = require("slugify");
 
+  function convert(url) {
+    return `/${slugify(url)}`;
+  }
+
   //NodeBB list of Hooks: https://github.com/NodeBB/NodeBB/wiki/Hooks
   Plugin.hooks = {
     filters: {
       categoryCreate: function(categoryData, callback) {
-        categoryData.category.slug = slugify(categoryData.category.slug);
+        categoryData.category.slug = convert(categoryData.category.slug);
         callback(null, categoryData);
       },
 
       categoryUpdate: function(categoryData, callback) {
         if ("slug" in categoryData.category) {
-          categoryData.category.slug = slugify(categoryData.category.slug);
+          categoryData.category.slug = convert(categoryData.category.slug);
         }
         callback(null, categoryData);
       },
 
       topicCreate: function(topicData, callback) {
-        topicData.topic.slug = slugify(topicData.topic.slug);
+        topicData.topic.slug = convert(topicData.topic.slug);
         callback(null, topicData);
       },
 
       topicEdit: function(topicData, callback) {
         //Here was a problem
-        topicData.topic.slug = slugify(topicData.topic.slug);
+        topicData.topic.slug = convert(topicData.topic.slug);
         callback(null, topicData);
       },
 
       userCreate: function(userData, callback) {
         //If there will be username collision, userslug will be overridden by NodeBB...
-        userData.userslug = slugify(userData.userslug);
+        userData.userslug = convert(userData.userslug);
         callback(null, userData);
       }
     }
